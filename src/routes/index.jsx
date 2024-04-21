25242c3430c58fd3108603f428dd0ef9f9a19da8
Gabriel <gabrielgapseu@gmail.com>
2024-04-21 13:25:13 -0300

    Feat: login page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from "react-router-dom"
+import Login from "../pages/Login";
 
 const AppRoutes = () => {
     return(
         <Routes>
-            
+            <Route path="/login" element={<Login/>} />
         </Routes>
     )
 }
