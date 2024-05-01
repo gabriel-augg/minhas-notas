1ea76dd2eaaea1375a2a22d150be1720266836a9
Johnson <johnsonbarbosa@outlook.com>
2024-05-01 16:44:46 -0300

    refactor: routes/index.jsx to include Login component

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,16 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 import Register from "../pages/Register";
+import Login from "../pages/Login";
 import Home from "../pages/Home";
 
 const AppRoutes = () => {
-    return(
-        <Routes>
-            <Route path="/cadastrar" element={<Register/>} />
-            <Route path="/" element={<Home/>}/>
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/cadastrar" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+};
 
 export default AppRoutes;
