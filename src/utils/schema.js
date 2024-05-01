da70fb72bdd2154e24789a707c3f6f20a283f51b
Johnson <johnsonbarbosa@outlook.com>
2024-05-01 16:44:56 -0300

    refactor: schema.js to include Loginchema and fix formatting

--- src/utils/schema.js
+++ src/utils/schema.js
@@ -1,24 +1,28 @@
-import { z } from "zod"
+import { z } from "zod";
 
 const RSchema = z.object({
-    username: z.string()
-        .nonempty("O nome de usuário é obrigatório.")
-        .min(3, "O nome de usuário deve ter pelo menos 3 caracteres.")
-        .max(20, "O nome de usuário não pode ser maior que 20 caracteres."),
+  username: z
+    .string()
+    .nonempty("O nome de usuário é obrigatório.")
+    .min(3, "O nome de usuário deve ter pelo menos 3 caracteres.")
+    .max(20, "O nome de usuário não pode ser maior que 20 caracteres."),
 
-    email:
-        z.string()
-        .nonempty("O email é obrigatório.")
-        .email("Digite um email válido."),
+  email: z
+    .string()
+    .nonempty("O email é obrigatório.")
+    .email("Digite um email válido."),
 
-    password:
-        z.string()
-        .nonempty("O senha é obrigatória.")
-        .min(4, "A senha deve ter pelo menos 8 caracteres."),
+  password: z
+    .string()
+    .nonempty("O senha é obrigatória.")
+    .min(4, "A senha deve ter pelo menos 8 caracteres."),
 
-    confirmpassword:
-        z.string()
-        .nonempty("A confirmação de senha é obrigatória.")
-})
+  confirmpassword: z.string().nonempty("A confirmação de senha é obrigatória."),
+});
 
-export { RSchema }
+const Loginchema = z.object({
+  email: z.string().email("Digite um email válido."),
+  password: z.string().min(4, "A senha deve ter pelo menos 8 caracteres."),
+});
+
+export { RSchema, Loginchema };
