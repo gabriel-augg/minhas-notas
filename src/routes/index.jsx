9cc4b8bd40a1fb6f188258698be6e17242bd7e77
Gabriel <gabrielgapseu@gmail.com>
2024-05-01 12:04:54 -0300

    Feat: Private router

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import Register from "../pages/Register";
 import Home from "../pages/Home";
+import Private from "./Private";
 
 const AppRoutes = () => {
     return(
         <Routes>
             <Route path="/cadastrar" element={<Register/>} />
-            <Route path="/" element={<Home/>}/>
+            <Route path="/" element={ <Private><Home/></Private> }/>
         </Routes>
     )
 }
