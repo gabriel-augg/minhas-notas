99567c34a8f086618a921e0bcaedb31dc2e38ddc
Johnson <johnsonbarbosa@outlook.com>
2024-05-01 16:44:20 -0300

    refactor: useAuth hook to include signIn and signUp functionality

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,53 +1,57 @@
-import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import api from "../utils/api"
-import useRequest from "./useRequest"
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../utils/api";
+import useRequest from "./useRequest";
 
 const useAuth = () => {
-    const [authenticated, setAuthenticated] = useState(false)
-    const [user, setUser] = useState(null)
-    const navigate = useNavigate()
-    const { request } = useRequest()
+  const [authenticated, setAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+  const { request } = useRequest();
 
-    useEffect(()=>{
-        const token = localStorage.getItem("token")
-        if(token){
-            api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`
-            request("/users/check-user", {
-                method: "get"
-            })
-            .then(({data})=> {
-                setUser(data.user)
-                setAuthenticated(true)
-            })
-        }
-
-    },[])
-
-    async function authUser(token){
-        localStorage.setItem("token", JSON.stringify(token))
-        api.defaults.headers.Authorization = `Bearer ${token}`
-        request("/users/check-user", {
-            method:"get"
-        })
-        .then(({data}) => {
-            setUser(data.user)
-            setAuthenticated(true)
-            navigate("/")
-        })
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`;
+      request("/users/check-user", {
+        method: "get",
+      }).then(({ data }) => {
+        setUser(data.user);
+        setAuthenticated(true);
+      });
     }
+  }, []);
 
+  async function authUser(token) {
+    localStorage.setItem("token", JSON.stringify(token));
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+    request("/users/check-user", {
+      method: "get",
+    }).then(({ data }) => {
+      setUser(data.user);
+      setAuthenticated(true);
+      navigate("/");
+    });
+  }
 
-    async function signUp(user){
-        const { data } = await request("/auth/sign-up", {
-            method: "post",
-            data: user
-        })
-        await authUser(data.token)
-    }
+  async function signIn(body) {
+    const { data } = await request("/auth/sign-in", {
+      method: "post",
+      data: body,
+    });
 
-    return { user, authenticated, signUp }
+    await authUser(data.token);
+  }
 
-}
+  async function signUp(user) {
+    const { data } = await request("/auth/sign-up", {
+      method: "post",
+      data: user,
+    });
+    await authUser(data.token);
+  }
+
+  return { user, authenticated, signUp, signIn };
+};
 
 export default useAuth;
