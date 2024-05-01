9e8600393902174f9fcb9f1186477874bc6b4edf
Gabriel <gabrielgapseu@gmail.com>
2024-05-01 14:32:27 -0300

    Style: change on style

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -27,13 +27,15 @@ export default function Note({id, title, description, tag, pinned}){
     return(
         <article onClick={handleShowModal} className="bg-lime-200 p-4 rounded-xl cursor-pointer border hover:border-lime-500 drop-shadow-md h-fit min-h-28">
             <div className="flex justify-between">
-                <h1 className="text-black font-bold text-xl mb-3">{title}</h1>
+                <h1 className="text-black font-bold text-xl mb-3 break-all">{title}</h1>
                 {pinned && (
-                    <RiPushpin2Fill className="text-lime-800" size={25} />
+                    <span>
+                        <RiPushpin2Fill className="text-lime-800" size={25} />
+                    </span>
                 )}
             </div>
             {!title && !description && <h1 className="text-lime-700 font-bold text-xl">Nota vazia</h1>}
-            <p className="text-black text-md mb-2">{description}</p>
+            <p className="text-black text-md mb-2 break-all">{description}</p>
 
             {tag && (
                 <div className="flex justify-end mt-5">
