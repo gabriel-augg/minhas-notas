27e5699e13c01cf36c8306ec4f22087b1c8b0382
Gabriel <gabrielgapseu@gmail.com>
2024-05-12 16:51:49 -0300

    Feat: creation of LoadingContent component

---
+++ src/components/LoadingContent.jsx
@@ -0,0 +1,7 @@
+export default function LoadingContent() {
+    return (
+        <div className="flex justify-center">
+            <span className="loading loading-bars bg-lime-200 loading-lg"></span>
+        </div>
+    );
+}
